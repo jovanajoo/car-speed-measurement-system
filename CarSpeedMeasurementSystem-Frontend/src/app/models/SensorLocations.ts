09312ffc5ce3c5ea1d9dd343f94c93cf5c74ebd4
Milan Tepic <milan.tepic221@gmail.com>
2022-06-08 19:01:58 +0200

    model

---
+++ CarSpeedMeasurementSystem-Frontend/src/app/models/SensorLocations.ts
@@ -0,0 +1,11 @@
+export class SensorLocation {
+    entryNo?: number;
+    startDate?: Date;
+    latitude?: number;
+    longitude?: number;
+    active?: boolean;
+    maxSpeed?: number;
+    endDate?: Date;
+    description?: string;
+    sensorSerialNumber?: number;
+}
